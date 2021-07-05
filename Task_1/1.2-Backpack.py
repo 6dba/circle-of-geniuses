c272c53c7b3a6d1d19732ed89cd8c220512c3c63
6dba <pasha.panukov@gmail.com>
2021-07-05 18:42:25 +0300

    Update Task_1.md

---
+++ Task_1/1.2-Backpack.py
@@ -0,0 +1,24 @@
+# 'Название':(стоимость, вес)
+THIGS = {
+    'bed':(220,70), 
+    'closet':(200,50), 
+    'desk':(130,22), 
+    'table':(300,20),
+    'tv':(200,30),
+    'chair':(100,5),
+    'bookshelf':(200,30), 
+    'cabinet':(150,20),
+    'game_table':(150,12),
+    'hammock':(250,45),
+    'diner_table':(55,7),
+    'mirror':(100,10),
+    'instrument':(300,70),
+    'plant':(25,10),
+    'standing_lamp':(20,5), 
+    'garbage_can':(30,3), 
+    'laptop':(500,2), 
+    'bike_stand':(75,10),
+    'chest':(150,25),
+    'heater':(30, 2),
+    'cat':(100, 5)
+}
