43ef812e19e7aeb396d939c3873d9f6e01eee268
6dba <pasha.panukov@gmail.com>
2021-07-08 16:50:55 +0300

    1.4 Stack realisation

---
+++ Task_1/1.4-Abstraction_and_Data_Structures.py
@@ -0,0 +1,52 @@
+from typing import Any
+
+
+class stack:
+
+    __stack = []
+
+    def push(self, data) -> None:
+        """
+        Занесение элемента в стек. Размер увеличивается на единицу, элемент становится вершиной
+        """
+        self.__stack.append(data)
+    
+    def pop(self) -> Any:
+        """
+        Извлечь элемент, являющийся вершиной стека и умнешьить размер на единицу
+        """
+        try:
+            item = self.__stack[-1]
+        except IndexError:
+            return
+        
+        self.__stack.__delitem__(-1)
+        
+        return item
+
+    def peek(self) -> Any:
+        """
+        Получить значение элемента, находящегося на вершине стека, не нарушая его целостности
+        """
+        try:
+            return self.__stack[-1]
+        except IndexError:
+            return
+
+    def empty(self) -> bool:
+        """
+        Предикат истинен, когда стек пуст
+        """
+        return not bool(self.__stack.__len__())
+
+    def destroy(self) -> None:
+        """
+        Разрушить стек
+        """
+        self.__stack = []
+
+    def __str__(self) -> str:
+        """
+        Метод печати стека
+        """
+        return self.__stack.__str__()
